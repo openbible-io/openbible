316870591acd34a0a43ff40d77f64d8c6d537320
thesmartwon <thesmartwon@live.com>
2022-06-14 01:18:32 -0400

    cleanup

--- scripts/build/index.js
+++ scripts/build/index.js
@@ -1,11 +1,9 @@
 import { copy } from './copy.js'
 import { render } from '../html/render.js'
-import { clean } from '../clean.js'
 import { js } from './js.js'
 import { fileURLToPath } from 'url'
 
 export async function build() {
-  clean()
   copy()
   const emitted = await js()
   render(emitted)
