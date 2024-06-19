e6c1e4505c782788d2285d6785cfe432b5dbd8ec
thesmartwon <thesmartwon@pm.me>
2024-06-19 16:55:13 -0400

    touchup about and checkbox

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,11 +2,11 @@ import { repository } from '../../package.json';
 
 export function About() {
 	return (
-		<>
+		<div>
 			<h2>About</h2>
 			<ul>
 				<li><a target="_blank" href={repository as unknown as string}>GitHub</a></li>
 			</ul>
-		</>
+		</div>
 	);
 }
