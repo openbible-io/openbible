e90705b83f06a4c0c7f1980d781bc7b2417b96f0
thesmartwon <thesmartwon@pm.me>
2025-03-06 21:09:00 -0500

    move find to diff2

--- shareword/src/egwalker/branch.ts
+++ shareword/src/egwalker/branch.ts
@@ -7,14 +7,13 @@ export class Branch<T> {
 	frontier: Clock[] = [];
 
 	checkoutFancy(oplog: OpLog<T>, mergeFrontier: Clock[] = oplog.frontier) {
-		const { commonVersion, sharedOps, bOnlyOps } = findOpsToVisit(
-			oplog,
+		const { head, shared, bOnly } = oplog.diff2(
 			this.frontier,
 			mergeFrontier,
 		);
 
 		const doc = new EgWalker();
-		doc.currentVersion = commonVersion;
+		doc.currentVersion = head;
 
 		const placeholderLength = Math.max(...this.frontier) + 1;
 		for (let i = 0; i < placeholderLength; i++) {
@@ -29,101 +28,11 @@ export class Branch<T> {
 			doc.itemsByClock[item.clock] = item;
 		}
 
-		for (const lv of sharedOps) doc.doOp(oplog, lv);
+		for (const lc of shared) doc.doOp(oplog, lc);
 
-		for (const lc of bOnlyOps) {
+		for (const lc of bOnly) {
 			doc.doOp(oplog, lc, this.snapshot);
 			this.frontier = advanceFrontier(this.frontier, lc, oplog.get(lc).parents);
 		}
 	}
-}
-
-function compareClocks(a: Clock[], b: Clock[]): number {
-	for (let i = 0; i < a.length; i++) {
-		if (b.length <= i) return 1;
-
-		const delta = a[i] - b[i];
-		if (delta !== 0) return delta;
-	}
-
-	if (a.length < b.length) return -1;
-	return 0;
-}
-
-type OpsToVisit = {
-	commonVersion: Clock[];
-	sharedOps: Clock[];
-	bOnlyOps: Clock[];
-};
-
-function findOpsToVisit(oplog: OpLog<any>, a: Clock[], b: Clock[]): OpsToVisit {
-	// if (a.length === 0 && b.length === 0) return { start: [], common: [], bOnly: [] }
-
-	type MergePoint = {
-		v: Clock[]; // Sorted in inverse order (highest to lowest)
-		isInA: boolean;
-	};
-
-	const queue = new PriorityQueue<MergePoint>((a, b) => {
-		// What about when they have different isInA flags? It shouldn't matter.
-		return compareClocks(b.v, a.v);
-	});
-
-	const enq = (lv: Clock[], isInA: boolean) => {
-		const mergePoint = {
-			v: lv.slice().sort((a, b) => b - a), // Sort in descending order.
-			isInA,
-		};
-		queue.push(mergePoint);
-	};
-
-	enq(a, true);
-	enq(b, false);
-
-	let commonVersion: Clock[];
-	const sharedOps = [];
-	const bOnlyOps = [];
-
-	// console.log('a', a, 'b', b)
-	while (true) {
-		let { v, isInA } = queue.pop()!;
-		// console.log('deq', v, isInA)
-		if (v.length === 0) {
-			// We've hit the root element.
-			commonVersion = [];
-			break;
-		}
-
-		while (!queue.isEmpty()) {
-			// We might have multiple elements that have the same merge point.
-			const { v: peekV, isInA: peekIsInA } = queue.peek()!;
-			if (compareClocks(v, peekV) !== 0) break;
-
-			queue.pop();
-			if (peekIsInA) isInA = true;
-		}
-
-		if (queue.isEmpty()) {
-			commonVersion = v.reverse();
-			break;
-		}
-
-		if (v.length >= 2) {
-			for (const vv of v) enq([vv], isInA);
-		} else {
-			const lc = v[0];
-			//assert(v.length == 1);
-			if (isInA) sharedOps.push(lc);
-			else bOnlyOps.push(lc);
-
-			const op = oplog.get(lc);
-			enq(op.parents, isInA);
-		}
-	}
-
-	return {
-		commonVersion,
-		sharedOps: sharedOps.reverse(),
-		bOnlyOps: bOnlyOps.reverse(),
-	};
 }
--- shareword/src/egwalker/oplog.ts
+++ shareword/src/egwalker/oplog.ts
@@ -1,4 +1,3 @@
-import { EgWalker } from "./egwalker";
 import PriorityQueue from "./pq";
 
 /** A collaborating agent */
@@ -18,6 +17,16 @@ export type Op<T> = {
 	id: Id;
 	parents: Clock[];
 };
+
+export function advanceFrontier(
+	frontier: Clock[],
+	clock: Clock,
+	parents: Clock[],
+): Clock[] {
+	const f = frontier.filter((v) => !parents.includes(v));
+	f.push(clock);
+	return f.sort((a, b) => a - b);
+}
 
 /** An append-only list of immutable operations, similar to Git */
 export class OpLog<T> {
@@ -88,21 +97,20 @@ export class OpLog<T> {
 
 	diff(a: Clock[], b: Clock[]): { aOnly: Clock[]; bOnly: Clock[] } {
 		type DiffFlag = "a" | "b" | "both";
-		const flags = new Map<Clock, DiffFlag>();
+		const flags: { [clock: Clock] : DiffFlag } = {};
 
 		let numShared = 0;
 
 		const queue = new PriorityQueue<Clock>((a, b) => b - a);
 
 		function enq(v: Clock, flag: DiffFlag) {
-			// Queue v, with the specified flag.
-			const oldFlag = flags.get(v);
+			const oldFlag = flags[v];
 			if (oldFlag == null) {
 				queue.push(v);
-				flags.set(v, flag);
+				flags[v] = flag;
 				if (flag === "both") numShared++;
 			} else if (flag !== oldFlag && oldFlag !== "both") {
-				flags.set(v, "both");
+				flags[v] = "both";
 				numShared++;
 			}
 		}
@@ -114,10 +122,9 @@ export class OpLog<T> {
 		const bOnly: Clock[] = [];
 
 		while (queue.size() > numShared) {
-			// biome-ignore lint/style/noNonNullAssertion: <explanation>
+			// biome-ignore lint/style/noNonNullAssertion: size check above
 			const clock = queue.pop()!;
-			// biome-ignore lint/style/noNonNullAssertion: <explanation>
-			const flag = flags.get(clock)!;
+			const flag = flags[clock];
 
 			if (flag === "a") aOnly.push(clock);
 			else if (flag === "b") bOnly.push(clock);
@@ -129,14 +136,86 @@ export class OpLog<T> {
 
 		return { aOnly, bOnly };
 	}
+
+	diff2(
+		a: Clock[],
+		b: Clock[],
+	): {
+		head: Clock[];
+		shared: Clock[];
+		bOnly: Clock[];
+	} {
+		type MergePoint = {
+			clocks: LocalClock[];
+			inA: boolean;
+		};
+
+		const queue = new PriorityQueue<MergePoint>((a, b) =>
+			cmpClocks(b.clocks, a.clocks),
+		);
+
+		const enq = (localClocks: LocalClock[], inA: boolean) => {
+			queue.push({
+				clocks: localClocks.toSorted((a, b) => b - a),
+				inA,
+			});
+		};
+
+		enq(a, true);
+		enq(b, false);
+
+		let head: Clock[] = [];
+		const shared = [];
+		const bOnly = [];
+
+		let next: MergePoint | undefined;
+		while ((next = queue.pop())) {
+			if (next.clocks.length === 0) break; // root element
+			let inA = next.inA;
+
+			let peek: MergePoint | undefined;
+			// multiple elements may have same merge point
+			while ((peek = queue.peek())) {
+				if (cmpClocks(next.clocks, peek.clocks)) break;
+
+				queue.pop();
+				if (peek.inA) inA = true;
+			}
+
+			if (queue.isEmpty()) {
+				head = next.clocks.reverse();
+				break;
+			}
+
+			if (next.clocks.length >= 2) {
+				for (const lc of next.clocks) enq([lc], inA);
+			} else {
+				const lc = next.clocks[0];
+				//assert(v.length == 1);
+				if (inA) shared.push(lc);
+				else bOnly.push(lc);
+
+				const op = this.get(lc);
+				enq(op.parents, inA);
+			}
+		}
+
+		return {
+			head,
+			shared: shared.reverse(),
+			bOnly: bOnly.reverse(),
+		};
+	}
 }
 
-export function advanceFrontier(
-	frontier: Clock[],
-	clock: Clock,
-	parents: Clock[],
-): Clock[] {
-	const f = frontier.filter((v) => !parents.includes(v));
-	f.push(clock);
-	return f.sort((a, b) => a - b);
+function cmpClocks(a: Clock[], b: Clock[]): number {
+	for (let i = 0; i < a.length; i++) {
+		if (b.length <= i) return 1;
+
+		const delta = a[i] - b[i];
+		if (delta !== 0) return delta;
+	}
+
+	if (a.length < b.length) return -1;
+	return 0;
 }
